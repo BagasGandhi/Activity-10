9d7df0da09bafae03593752fca7bcf01709e5fc8
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-05-12 15:31:43 +0700

    Implement methods for adding and printing children in ibu class

--- ibu.h
+++ ibu.h
@@ -24,4 +24,23 @@ public:
         cout << "ibu \"" << nama << "\" tidak ada \n";
     };
 
+    //deklarasi prosedure tambahAnak() dan cetakAnak()
+    void tambahAnak(anak* pAnak);
+    void cetakAnak();
 };
+
+// pendefinisian prosedure - prosedure pada class ibu diluar class
+void ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+
+void ibu::cetakAnak() {
+    cout << "Daftar Anak dari ibu \"" << this-> nama << "\": \n";
+    for (auto& a : daftar_anak) 
+    {
+        cout << " - " << a->nama << "\n";
+    }
+    cout << endl;
+}
+
+#endif
