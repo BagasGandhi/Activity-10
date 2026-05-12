c6221469406d69e836643368732267c736ea349a
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-05-12 16:07:30 +0700

    Refactor: Restore anak class header file and implement its constructor and destructor

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -5,4 +5,17 @@ using namespace std;
 //pembuatan class orangtua
 class orang {
 
+public:
+    //deklarasi member variabel dari class orang
+    string nama;
+
+    //pembuatan constructor dan destructor pada class orang
+    orang(string pNama) : nama(pNama) {
+        cout << "Orang dibuat \n" << endl;
+    }
+
+    ~orang() {
+        cout << "Orang dihapus \n" << endl;
+    }
+
 };
