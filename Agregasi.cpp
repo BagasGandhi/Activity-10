206e8aa5709121b19974920fc9956d495f98f200
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-05-12 15:36:27 +0700

    Refactor: Reintroduce anak class header file and update its implementation in Agregasi.cpp

--- Agregasi.cpp
+++ Agregasi.cpp
@@ -7,3 +7,13 @@ using namespace std;
 #include "ibu.h"
 #include "anak.h"
 
+int main()
+{
+
+    //deklarasi dan pemberian nilai variable pointer untuk objek dari class ibu dan anak
+    ibu* varIbu = new ibu("rani");
+    ibu* varIbu2 = new ibu("sari");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("tiara");
+    anak* varAnak3 = new anak("dini");
+}
